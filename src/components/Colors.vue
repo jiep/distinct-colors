00fb9f2fca90a2af6714a861ec7bcb83c57325ee
José Ignacio Escribano <jiep@users.noreply.github.com>
2023-08-01 15:10:24 +0000

    First version

---
+++ src/components/Colors.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{ colors: Array<string> }>()
+</script>
+
+<template>
+  <section class="container sm:mx-auto p-8 grid lg:grid-cols-6 grid-cols-2 gap-x-12 gap-y-8">
+    <div class="rounded-lg text-center border flex flex-row items-center" v-for="color in colors">
+      <span class="flex-shrink-0 rounded-l-lg w-1/5 h-12" :style="{backgroundColor: color}"></span>
+      <span class="flex-shrink px-2 w-full text-gray-500 tabular-nums text-center select-all">{{ color }}</span>
+      <span class="flex-shrink-0 rounded-r-lg w-1/5 h-12" :style="{backgroundColor: color}"></span>
+    </div>
+  </section>
+</template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav class="bg-sky-300">
+      <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div class="relative flex h-16 items-center justify-between">
+          <div class="flex flex-1 items-center justify-center">
+            <div class="flex flex-shrink-0 items-center">
+              <img src='/logo.png' alt="Logo" class="h-12 w-auto mr-4" />
+              <span class="text-white text-2xl font-semibold">Distinct colors</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+</template>
