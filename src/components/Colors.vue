4493bf6d929da648c51d8a67a8ba1b79da442375
José Ignacio Escribano <jiep@users.noreply.github.com>
2025-04-11 14:11:05 +0000

    Migrate to Tailwind v4

--- src/components/Colors.vue
+++ src/components/Colors.vue
@@ -5,9 +5,9 @@ defineProps<{ colors: Array<string> }>()
 <template>
   <section class="container sm:mx-auto p-8 grid lg:grid-cols-6 grid-cols-2 gap-x-12 gap-y-8">
     <div class="rounded-lg text-center border flex flex-row items-center" v-for="color in colors">
-      <span class="flex-shrink-0 rounded-l-lg w-1/5 h-12" :style="{backgroundColor: color}"></span>
-      <span class="flex-shrink px-2 w-full text-gray-500 tabular-nums text-center select-all">{{ color }}</span>
-      <span class="flex-shrink-0 rounded-r-lg w-1/5 h-12" :style="{backgroundColor: color}"></span>
+      <span class="shrink-0 rounded-l-lg w-1/5 h-12" :style="{backgroundColor: color}"></span>
+      <span class="shrink px-2 w-full text-gray-500 tabular-nums text-center select-all">{{ color }}</span>
+      <span class="shrink-0 rounded-r-lg w-1/5 h-12" :style="{backgroundColor: color}"></span>
     </div>
   </section>
 </template>
